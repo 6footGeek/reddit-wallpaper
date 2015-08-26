6d8708c2e1c0291ca15e00040b4551b549c8815d
Andy <6footgeek@gmail.com>
2015-08-26 16:23:55 +0100

    added karma and a gitignore

---
+++ spec/main-spec.js
@@ -0,0 +1,23 @@
+var request = require( 'request' );
+describe( 'that we can connect to reddit', function() {
+
+    it( "should connect to reddit top minimal wallpapers", function( done ) {
+        request( "https://www.reddit.com/r/MinimalWallpaper/top/.json", function( error, response ) {
+            expect( response.statusCode ).toEqual( 200 );
+            done();
+        } );
+    } );
+
+
+
+
+
+
+} );
+
+
+describe('general functionality', function() {
+	// it( 'should get the highest rated wallpaper and save it in a folder in pictures', function( error, response ) {
+
+	// });
+});
