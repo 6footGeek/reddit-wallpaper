3ba87d6c69e6d48e622e21e3b312e73bc9b93cdb
Andy <6footgeek@gmail.com>
2015-08-26 18:35:35 +0100

    reverted

--- my.conf.js
+++ my.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration
-// Generated on Wed Aug 26 2015 16:18:34 GMT+0100 (BST)
-
 module.exports = function(config) {
   config.set({
 
@@ -15,7 +12,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dist/*.js , spec/*.js'
+      'dist/*.js' , 'tests/*.js'
     ],
 
 
@@ -55,11 +52,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Safari', 'Chrome'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  })
-}
+  });
+};
