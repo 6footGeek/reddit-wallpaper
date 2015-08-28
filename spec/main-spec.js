81393b301d17526328e252a04dc6e23e2b6328df
Andy <6footgeek@gmail.com>
2015-08-28 20:26:01 +0100

    fixed

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,18 @@
-describe( 'a set of tests ', function() {
-    it( "should probably test some stuff here", function( done ) {
-        expect( 1 + 1 ).toEqual( 2 );
+var request = require( 'request' );
+
+describe( 'that we can connect to reddit', function() {
+
+    it( "should connect to reddit top minimal wallpapers", function( done ) {
+        request( "https://www.reddit.com/r/MinimalWallpaper/top/.json", function( error, response ) {
+            expect( response.statusCode ).toEqual( 200 );
+            done();
+        } );
     } );
 } );
+
+
+describe('general functionality', function() {
+	// it( 'should get the highest rated wallpaper and save it in a folder in pictures', function( error, response ) {
+
+	// });
+});
